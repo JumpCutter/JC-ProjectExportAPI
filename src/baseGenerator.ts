b78ad4d8104ffa3e5785d8e4169e9608990176fd
dk949 <56653556+dk949@users.noreply.github.com>
2021-10-01 21:53:20 +0100

    fix(base): fixed variables persist between derived objects (???)

--- src/baseGenerator.ts
+++ src/baseGenerator.ts
@@ -1,58 +1,61 @@
 import {Project, Layer, Cuts} from "./project";
 export abstract class Generator {
 
-  private project: Project;
-  protected layer: Layer;
-  protected cuts: Cuts;
-  protected frameRate: number;
-  protected clipName: string;
-  constructor(project: Project) {
-    this.project = project;
-    this.layer = this.ensureSingleLayer(this.project);
-    this.cuts = this.fillInGaps(this.ensureCuts(this.layer));
-    this.layer.cuts = this.cuts;
-    this.project.layers![0]![0] = this.layer; // we alreaady know it isn't null
-    this.frameRate = this.project.frameRate;
-    this.clipName = this.layer.sourceFile;
-  }
+    private project: Project;
+    protected layer: Layer;
+    protected cuts: Cuts;
+    protected frameRate: number;
+    protected clipName: string;
 
 
-  private ensureSingleLayer(project: Project): Layer {
-    const layers = project.layers;
-    if (layers == null || layers == undefined) {
-      throw new Error("No layers!!!");
+    constructor(project: Project) {
+        this.project = project;
+        this.layer = this.ensureSingleLayer(this.project);
+        this.cuts = this.fillInGaps(this.ensureCuts(this.layer));
+        //this.layer.cuts = this.cuts;
+        //this.project.layers![0]![0] = this.layer; // we already know it isn't null
+        this.frameRate = this.project.frameRate;
+        this.clipName = this.layer.sourceFile;
     }
-    if (layers[0] == null || layers[0] == undefined) {
-      throw new Error("No layers inside of the layers!!!");
-    }
-    if (layers[0][0] == null || layers[0][0] == undefined) {
-      throw new Error("No layers inside of the layers insode of the layers?!!!");
-    }
-    if (layers.length > 1 || layers[0].length > 1) {
-      throw new Error("What do I do with this many layers?");
-    }
-    return layers[0][0];
-  }
-
-  private ensureCuts(layer: Layer): Cuts {
-    if (layer.cuts == null || layer.cuts == undefined) {
-      throw new Error("No cuts");
-    }
-    return layer.cuts;
-  }
-
-  private fillInGaps(cuts: Cuts): Cuts {
-    const newCuts: Cuts = [];
-    for (let i: number = 0; i < cuts.length; i++) {
-      newCuts.push(cuts[i]);
-      if (i + 1 < cuts.length) {
-        newCuts.push({start: cuts[i].end, end: cuts[i + 1].start});
-      }
-    }
-    return newCuts;
-  }
-
-  abstract generate(): string;
 
 
+    private ensureSingleLayer(project: Project): Layer {
+        const layers = project.layers;
+        if (layers == null || layers == undefined) {
+            throw new Error("No layers!!!");
+        }
+        if (layers[0] == null || layers[0] == undefined) {
+            throw new Error("No layers inside of the layers!!!");
+        }
+        if (layers[0][0] == null || layers[0][0] == undefined) {
+            throw new Error("No layers inside of the layers insode of the layers?!!!");
+        }
+        if (layers.length > 1 || layers[0].length > 1) {
+            throw new Error("What do I do with this many layers?");
+        }
+        return layers[0][0];
+    }
+
+    private ensureCuts(layer: Layer): Cuts {
+        if (layer.cuts == null || layer.cuts == undefined) {
+            throw new Error("No cuts");
+        }
+        return layer.cuts;
+    }
+
+    private fillInGaps(cuts: Cuts): Cuts {
+        const newCuts: Cuts = [];
+        for (let i: number = 0; i < cuts.length; i++) {
+            newCuts.push(cuts[i]);
+            if (i + 1 < cuts.length) {
+                newCuts.push({
+                    start: cuts[i].end,
+                    end: cuts[i + 1].start
+                });
+            }
+        }
+        return newCuts;
+    }
+
+    abstract generate(): string;
 };
