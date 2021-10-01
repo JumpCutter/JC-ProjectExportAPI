edd999c154877966a52e5996b47a5906cba5d6f1
dk949 <56653556+dk949@users.noreply.github.com>
2021-10-01 21:51:42 +0100

    refactor(EDL): reformatted EDL

--- src/EDL/genEDL.ts
+++ src/EDL/genEDL.ts
@@ -6,20 +6,20 @@ import path from "path";
 
 
 export default class ResolevEdl extends Generator {
-  generate() {
-    const edlIr = new EDL({
-      "title": path.parse(this.clipName).name,
-      "events": this.cuts.map((cut: Cut, i: number) => {
-        return {
-          "id": i,
-          "startTime": cut.start,
-          "endTime": cut.end,
-          "reelName": "N/A",
-          "clipName": this.clipName,
-          "fps": this.frameRate
-        }
-      })
-    });
-    return edlIr.compose();
-  }
+    generate() {
+        const edlIr = new EDL({
+            "title": "JumpCutterExport",
+            "events": this.cuts.map((cut: Cut, i: number) => {
+                return {
+                    "id": i,
+                    "startTime": cut.start,
+                    "endTime": cut.end,
+                    "reelName": "N/A",
+                    "clipName": this.clipName,
+                    "fps": this.frameRate
+                }
+            })
+        });
+        return edlIr.compose();
+    }
 }
