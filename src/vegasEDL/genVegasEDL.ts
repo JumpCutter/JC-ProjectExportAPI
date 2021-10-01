11adbf8cc1926583e0fc13baf328b016858834ed
dk949 <56653556+dk949@users.noreply.github.com>
2021-10-01 18:38:46 +0100

    fix(vegasEDL): refactored to use a builder

---
+++ src/vegasEDL/genVegasEDL.ts
@@ -0,0 +1,31 @@
+import {Generator} from "../baseGenerator";
+import {vegasEDLBuilder} from "./helpersVegasEDL";
+
+export default class vegasEDL extends Generator {
+
+  // the main thing
+  generate() {
+    const builder = new vegasEDLBuilder();
+    let idCounter = 0;
+    builder.mediaTypes.forEach((mtype, i) => {
+      this.cuts.forEach((cut) => {
+        builder.buildContextManager(() => {
+          builder
+            .putID(++idCounter)
+            .putTrack(i)
+            .putStartTime(cut.start)
+            .putLength(cut.end - cut.start)
+            .putFirstDefualts()
+            .putMediaType(mtype)
+            .putFileName(this.clipName)
+            .putStream()
+            .putStreamStart(cut.start)
+            .putStreamLength(cut.end - cut.start)
+            .putSecondDefualts();
+        });
+      })
+    })
+    return builder.data;
+  }
+}
+
