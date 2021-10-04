a42fd14c1ff335bb1e153c9c4c15fcc4408e1c91
dk949 <56653556+dk949@users.noreply.github.com>
2021-10-04 13:24:18 +0100

    feat(all): added a definitions file

---
+++ src/ProjectExport.d.ts
@@ -0,0 +1,5 @@
+export {Generator} from "./baseGenerator";
+export {ResolveEDL} from "./EDL/genEDL";
+export {VegasEDL} from "./vegasEDL/genVegasEDL";
+export {FCPXML} from "./FCPXML/genFCPXML";
+export {XML} from "./XML/genXML";
